c16eb96b478144abff484919916725c6163d3b8c
Fabricio Curvello <fabricio.curvello@gmail.com>
2019-03-30 17:01:21 -0300

    Configuracoes iniciais de Retrofit e YouTube Apis

--- app/src/main/java/com/project/fabricio/algoritmosjava/helper/YouTubeConfig.java
+++ app/src/main/java/com/project/fabricio/algoritmosjava/helper/YouTubeConfig.java
@@ -1,4 +1,11 @@
 package com.project.fabricio.algoritmosjava.helper;
 
+import androidx.annotation.Nullable;
+
 public class YouTubeConfig {
+
+    public static String CANAL_ID = "";
+    public static String CHAVE_YOUTUBE_API = "AIzaSyD4z9O_Ze-wdxXtYqHSwYX6zUZyZ4yj0G4";
+    public static String URL_BASE = "";
+
 }
