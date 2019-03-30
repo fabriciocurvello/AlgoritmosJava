91ac922f8ce8fc8791135fb3619c4fd8cc0f8910
Fabricio Curvello <fabricio.curvello@gmail.com>
2019-03-30 12:59:33 -0300

    Configuracoes iniciais de interface (toolbar e recyclerview)

--- app/src/main/java/com/project/fabricio/algoritmosjava/MainActivity.java
+++ app/src/main/java/com/project/fabricio/algoritmosjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.project.fabricio.algoritmosjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configurando Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Algorítmos em JAVA");
+        setSupportActionBar(toolbar);
     }
 }
