48a1a68b09c079b651101271761c74ab2d4e973f
Fabricio Curvello <fabricio.curvello@gmail.com>
2019-03-31 00:39:20 -0300

    Exibindo video

--- app/src/main/java/com/project/fabricio/algoritmosjava/activity/PlayerActivity.java
+++ app/src/main/java/com/project/fabricio/algoritmosjava/activity/PlayerActivity.java
@@ -4,13 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.project.fabricio.algoritmosjava.R;
+import com.project.fabricio.algoritmosjava.helper.YouTubeConfig;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView youTubePlayerView;
+    private String idVideo;
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean foiRestaurado) {
+
+        youTubePlayer.setFullscreen(true);
+        youTubePlayer.setShowFullscreenButton(false);
+        youTubePlayer.loadVideo( idVideo );
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        youTubePlayerView = findViewById(R.id.playerVideo);
+
+        //bundle recebe o conteúdo que foi passado pela Intent
+        Bundle bundle = getIntent().getExtras();
+
+        if ( bundle != null ) {
+            idVideo = bundle.getString( "idVideo" );
+            youTubePlayerView.initialize(YouTubeConfig.CHAVE_YOUTUBE_API, this);
+        }
+
+
+
     }
 }
